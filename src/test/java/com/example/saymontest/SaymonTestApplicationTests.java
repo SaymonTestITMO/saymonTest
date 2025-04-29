8954a948e4cf9ae8a18262c8dba408a8c71bccd7
naje1 <152160512+naje1@users.noreply.github.com>
2025-04-29 13:17:58 +0300

    Initial commit:pom added

---
+++ src/test/java/com/example/saymontest/SaymonTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.saymontest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaymonTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
