a06de78608dbd34ad0ddb92b1e2615fa5f628af2
naje1 <152160512+naje1@users.noreply.github.com>
2025-04-29 23:47:22 +0300

    Restore: pom.xml and main Application class

---
+++ src/main/java/com/example/saymontest/SaymonTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.saymontest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaymonTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaymonTestApplication.class, args);
+    }
+
+}
